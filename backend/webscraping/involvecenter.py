afe2ff031297368fde42831d8db31011be72cf40
Josh <joshua.stiles2002@gmail.com>
2025-03-11 20:44:04 -0700

    Added files for Involvement Center Events webscraper

---
+++ backend/webscraping/involvecenter.py
@@ -0,0 +1,41 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
+from time import sleep
+
+options = webdriver.ChromeOptions()
+driver = webdriver.Chrome(options=options)
+url = 'https://involvementcenter.unlv.edu/events'
+
+# Open the webpage
+driver.get(url)
+
+# Wait for webpage to load
+try:
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "react-app"))
+    )
+except TimeoutException:
+    driver.quit()
+
+# Click to load all events
+while True:
+    try:
+        driver.find_element(By.XPATH, '//span[text()="Load More"]').click()
+    except NoSuchElementException:
+        break
+
+# Retrieve events and dates
+events = driver.find_elements(By.TAG_NAME, 'h3')
+dates = driver.find_elements(By.XPATH, '//div[@style="margin: 0px 0px 0.125rem;"]')
+del events[:5]
+
+# Write events and dates to file
+with open('events.txt','w') as f:
+    for event, date in zip(events, dates):
+        f.write(event.text + "\n" + date.text + "\n\n")
+
+# Close webpage
+driver.quit()
