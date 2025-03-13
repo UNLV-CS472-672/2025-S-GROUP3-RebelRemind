3b9add420364486d55469af549b6378aec2d1ad1
Josh <42755234+StarStiles@users.noreply.github.com>
2025-03-13 10:24:29 -0700

    Resolved changes for PR #7

--- backend/webscraping/involvement_center.py
+++ backend/webscraping/involvement_center.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException, NoSuchElementException
+from selenium.common.exceptions import TimeoutException, NoSuchElementException, StaleElementReferenceException
 from time import sleep
 
 options = webdriver.ChromeOptions()
@@ -23,8 +23,9 @@ except TimeoutException:
 # Click to load all events
 while True:
     try:
-        driver.find_element(By.XPATH, '//span[text()="Load More"]').click()
-    except NoSuchElementException:
+        load_more = driver.find_element(By.XPATH, '//span[text()="Load More"]')
+        load_more.click()
+    except NoSuchElementException or StaleElementReferenceException:
         break
 
 # Retrieve events and dates
@@ -33,7 +34,7 @@ dates = driver.find_elements(By.XPATH, '//div[@style="margin: 0px 0px 0.125rem;"
 del events[:5]
 
 # Write events and dates to file
-with open('events.txt','w') as f:
+with open('involvement_center.txt','w') as f:
     for event, date in zip(events, dates):
         f.write(event.text + "\n" + date.text + "\n\n")
 
