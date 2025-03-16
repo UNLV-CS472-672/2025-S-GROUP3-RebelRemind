a6c62965d8656a64b7279578cc8622f1cb65934d
Jeremy B <besitula@unlv.nevada.edu>
2025-03-16 13:35:18 -0700

    Revised PR #1

--- rebel-extension/src/App.jsx
+++ rebel-extension/src/App.jsx
@@ -3,8 +3,7 @@ import { useEffect, useState, useRef } from "react";
 import "./App.css";
 import LoginButton from "./components/LoginButton";
 import Counter from "./components/Counter";
-import Accordion from 'react-bootstrap/Accordion';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import AccordionMenu from "./components/AccordionMenu";
 
 /* This file defines the main UI layout for our Chrome extension’s popup or tab interface. */
 
@@ -13,29 +12,7 @@ function App() {
     <>
       <Counter />
       <LoginButton />
-      <Accordion defaultActiveKey={['0']} alwaysOpen>
-      <Accordion.Item eventKey="0">
-        <Accordion.Header>Upcoming Assignments</Accordion.Header>
-        <Accordion.Body>
-          • CS405 - Assignment 2 <br />
-          • CS422 - Assignment 3 <br />
-        </Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="1">
-        <Accordion.Header>Your Events</Accordion.Header>
-        <Accordion.Body>
-          • Kat Parsons (CS251X Office Hours) <br />
-          	&emsp;&emsp;&emsp; March 15th, 2025 - 1:00 PM
-        </Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="2">
-        <Accordion.Header>UNLV Events</Accordion.Header>
-        <Accordion.Body>
-          • UNLV Volleyball Tryouts: <br />
-          	&emsp;&emsp;&emsp; March 31st, 2025 - 4:00 PM
-        </Accordion.Body>
-      </Accordion.Item>
-    </Accordion>
+      <AccordionMenu />
     </>
     
   );
