8ea8654475be90aca45c53cf44e95f25d151d633
Billy Estrada <120008652+billy-estrada@users.noreply.github.com>
2025-03-18 16:11:15 -0700

    Fixing issues again

--- rebel-extension/src/tests/Counter.test.jsx
+++ rebel-extension/src/tests/Counter.test.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, screen, fireEvent, waitFor } from "@testing-library/react";
-import Counter from "./Counter";
+import Counter from "../components/Counter";
 
 // Mock Chrome's runtime messaging API
 beforeAll(() => {
