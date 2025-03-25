cf6cf4dd7d2a786375fbe62bcf73aebdbd29a201
Sebastian Yepez <121584176+SebastianYepez@users.noreply.github.com>
2025-03-25 15:36:19 -0700

    Custom Event Page finished

--- rebel-extension/src/App.jsx
+++ rebel-extension/src/App.jsx
@@ -4,7 +4,11 @@ import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import SettingPage from "./pages/SettingPage";
 import useApplyBackgroundColor from "./hooks/useApplyBackgroundColor";
+import UserEventsPage from "./pages/UserEventsPage"
 
+/**
+ * Main UI Layout for the Chrome Extension.
+ */
 function App() {
   useApplyBackgroundColor();
 
@@ -13,6 +17,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/settings" element={<SettingPage />} />
+        <Route path="/user-events" element={<UserEventsPage />} />
       </Routes>
     </Router>
   );
