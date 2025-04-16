7f947336afc768634d301f9e7862918619df8a6e
chandni109 <167682066+chandni109@users.noreply.github.com>
2025-04-15 17:26:47 -0700

    I added the accordion menu to stay open always and scrollable to see the rest of the list.

--- rebel-extension/src/components/AccordionMenu.jsx
+++ rebel-extension/src/components/AccordionMenu.jsx
@@ -21,11 +21,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
  */
 function AccordionMenu() {
   return (
-    <div>
-      <Accordion alwaysOpen>
+    <div className="accordion-scroll-wrapper">
+      <Accordion defaultActiveKey={["0", "1", "2"]} alwaysOpen>
       	<Accordion.Item eventKey="0">
       	<Accordion.Header>📚 Upcoming Assignments</Accordion.Header>
-          <Accordion.Body>
+          <Accordion.Body className="accordion-panel-scroll">
           {/* • <strong> 🗺️ History 405:</strong> Homework 3 due by this Sunday <strong> <br />
           • <strong> 💻 CS 472:</strong> DP II</strong> due by next week Tuesday. */}
           <CanvasAssignments>
@@ -34,16 +34,20 @@ function AccordionMenu() {
         </Accordion.Item>
         <Accordion.Item eventKey="1">
           <Accordion.Header>📅 Your Events</Accordion.Header>
-          <Accordion.Body>
+          <Accordion.Body className="accordion-panel-scroll">
           • 🗓️ General Meeting - <strong> 11:30 AM </strong> with <strong>Hindu Yuva Club</strong>. <br />
-          • 🕛 Office Hours - <strong>2:00 PM </strong> with the GOAT 🐐 Kishore.
+          • 🕛 Office Hours - <strong>2:00 PM </strong> with the GOAT 🐐 Kishore. <br />
+          • 🏀 UNLV <strong>Basketball Game - </strong> 5:00 PM <strong>!</strong>.  <br />
+          • 🕛 Office Hours - <strong>5:00 PM </strong> with the DR. Pedersen.
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="2">
           <Accordion.Header>🎉 UNLV Events</Accordion.Header>
-          <Accordion.Body>
+          <Accordion.Body className="accordion-panel-scroll">
           • 🏀 UNLV <strong>Basketball Game - </strong> 5:00 PM <strong>!</strong>. <br />
-          • 🐶 Pet therapy - 3:00 PM located in the <strong>Lied Library</strong>!
+          • 🐶 Pet therapy - 3:00 PM located in the <strong>Lied Library</strong>! <br />
+          🗓️ General Meeting - <strong> 11:30 AM </strong> with <strong>Hindu Yuva Club</strong>. <br />
+          • 🕛 Volleyball Tryouts - <strong>8:00 PM </strong> with Coach Tarina.
           </Accordion.Body>
         </Accordion.Item>
       </Accordion>
