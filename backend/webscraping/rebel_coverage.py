2576e8dbb251a3f7524a5fcd112215bd64cf340d
franklopez1981 <franklopez1981@gmail.com>
2025-04-19 08:11:56 -0700

    Modified Rebel Coverage scraper and database (serve_data.py) compatibility

--- backend/webscraping/rebel_coverage.py
+++ backend/webscraping/rebel_coverage.py
@@ -1,8 +1,9 @@
 import requests
 import json
 from bs4 import BeautifulSoup
-from database import BASE
 
+# from database import BASE
+BASE = "http://127.0.0.1:5050/"
 url = 'https://unlvrebels.com/coverage'
 
 def default():
@@ -13,7 +14,6 @@ def default():
         table = soup.find_all('table')[0]
         # Get the rows
         data = table.find_all('tr')[1:]
-
         # Print list of dates with their events
         weekdays = ['MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY', 'SUNDAY']
         data_table = []
@@ -21,16 +21,15 @@ def default():
         event_date = ''
 
         for item in data:
+            # get the sport type
+            if item.find_all("td", class_="hide-on-medium-down"):
+                sport = item.find_all("td", class_="hide-on-medium-down")[0].text
             time = item
             item = item.find('th').text
             dist = item.find('\n', 1)
             trunc_item = item[dist:].replace('\n','')
             item = item.replace('\n','', 0)
             item = item.replace('\n',' ')
-
-            link_tag = time.find('a', href=True)
-            link = link_tag['href'] if link_tag else None
-
             if any(day in item.upper() for day in weekdays):
                 event_date = trunc_item
             else:
@@ -38,15 +37,13 @@ def default():
                     time = time.find('td').text
                 else:
                     time = 0
-                data_table.append({"name": item, "date": event_date, "time": time, "link": link,})
-
-        # print(data_table)
+                data_table.append({"name": item, "date": event_date, "time": time, "sport": sport})
 
         for i in range(len(data_table)):
-            requests.put(BASE + f"rebelcoverage_add", json={"name": data_table[i]["name"], "date": data_table[i]["date"], "time": data_table[i]["time"], "link" : data_table[i]["link"],})
+            requests.put(BASE + f"rebelcoverage_add", json={"name": data_table[i]["name"], "date": data_table[i]["date"], "time": data_table[i]["time"], "sport": data_table[i]["sport"]})
         
-        with open('scraped_RebelCoverage.json', 'w') as json_file:
-            json.dump(data_table, json_file, indent=4)
+        # with open('scraped_RebelCoverage.json', 'w') as json_file:
+        #     json.dump(data_table, json_file, indent=4)
 
     else:
         print(f"Failed to access the page. Status code: {response.status_code}")
